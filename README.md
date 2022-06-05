0952e6fb5648b724f49b531e26af111e54203e1f
luizfelipevbll <luizfelipevbll@gmail.com>
2022-06-04 21:52:33 -0300

    feat: impl. base templates & deta basics and uvicorn wt routes

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# inpi-rpa-notifications
-Sist
+# **INPI** RPA Notifications
+
+Automation of **INPI** protocol update notifications.
+**INPI** requires the user who requested a service to access the [website](http://revistas.inpi.gov.br/rpi/) weekly to download the magazine and check the status of the protocol.
+
+## Requirements
+
+| Tech | Version |
+| --- | --- |
+| [Python](https://www.python.org/) | 3.9.5 |
+| [FastAPI](https://fastapi.tiangolo.com/) | 0.78.0 |
+| [Docker](https://docs.docker.com/) | 20.10.16 |
+| [Docker-Compose](https://docs.docker.com/compose/) | 2.5.0 |
+| [Deta.sh](https://docs.deta.sh/docs/home/) | 1.1.0 |
+| [Bootstrap](https://getbootstrap.com/docs/5.2/getting-started/introduction/) | 5.2 |
+
+
+## Running locally in development
+
+Using Docker Compose:
+
+```sh
+docker-compose build
+docker-compose up -d
+```
+
+Using VENV:
+
+```sh
+python -m venv venv
+source venv/bin/activate
+uvicorn main:app --reload
+```
+
+Project will be running on http://localhost:8000
+
+### .ENV file
+
+```sh
+PROJECT_KEY=""
+PROJECT_ID=""
+```
---
+++ main.py
@@ -0,0 +1,73 @@
+import secrets, os
+from deta import Deta
+# from deta import app as app_cron
+from fastapi import FastAPI, HTTPException, status, Depends, Request
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.templating import Jinja2Templates
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+__version__ = "0.0.1"
+PROJECT_KEY = os.environ.get("PROJECT_KEY", "")
+
+app = FastAPI(version=__version__, docs_url=False, redoc_url=False)
+deta = Deta(PROJECT_KEY)
+templates = Jinja2Templates(directory="templates")
+security = HTTPBasic()
+users_db = deta.Base("users")
+
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "admin")
+    correct_password = secrets.compare_digest(credentials.password, "admin")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+
+@app.exception_handler(status.HTTP_404_NOT_FOUND)
+async def custom_http_exception_handler(request, exc):
+    return RedirectResponse("/")
+
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/register")
+async def set_register(request: Request):
+    return RedirectResponse("/", status.HTTP_302_FOUND)
+
+
+@app.post("/login")
+async def set_login(request: Request):
+    return RedirectResponse("/", status.HTTP_302_FOUND)
+
+
+@app.post("/track-proccess")
+async def set_track_proccess(credentials: HTTPBasicCredentials = Depends(security)):
+    user = {"username": credentials.username, "password": credentials.password}
+    return RedirectResponse("/", status.HTTP_302_FOUND)
+
+
+@app.post("/unsubscribe/{email}")
+async def unsubscribe(request: Request, email: str = None):
+    # TODO remove email 
+    return RedirectResponse("/", status.HTTP_302_FOUND)
+
+
+# @app_cron.lib.cron()
+# def cron_job(event):
+
+#     file = drive.get(name)
+#     # TODO cron para verificar arquivos com determinado ID e enviar email notificando
+
+#     deta.send_email(to, subject, message)
+
+#     return "verifica arquivo recente e busca"
