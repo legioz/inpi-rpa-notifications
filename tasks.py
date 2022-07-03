6b241ed8b5baf98127fa5f972bc16948f8c8682b
luizfelipevbll <luizfelipevbll@gmail.com>
2022-07-03 07:29:43 -0300

    feat: download document

--- tasks.py
+++ tasks.py
@@ -1,4 +1,4 @@
-import time, os
+import time, os, zipfile
 from deta import Deta
 from pathlib import Path
 from RPA.Browser.Selenium import Selenium
@@ -38,19 +38,45 @@ def open_website(url):
     )
 
 
-def download_file():
-    """clear all files and download most recently file from INPI"""
+def download_file() -> str:
+    """download most recently file from INPI"""
+    browser_lib.wait_until_page_contains_element("//table[@class='table table-text-center table-condensed table-bordered table-middle']/tbody")
+    browser_lib.wait_until_element_is_visible("//table//tbody/tr[@class='warning']/td[7]/div/a[2]")
+    link_el = browser_lib.find_element("//table//tbody/tr[@class='warning']/td[7]/div/a[2]")
+    link_str = browser_lib.get_element_attribute(link_el, "href")
+    filename = str(link_str).split("/")[-1]
+    link_el.click()
+    while not Path(DOWNLOAD_DIR).joinpath(filename).is_file():
+        time.sleep(1)
+    print(" [x] file downloaded succefully")
+    browser_lib.close_window()
+    browser_lib.close_browser()
+    return filename
 
 
-def search_protocols():
+def search_protocols(file: str):
     """search protocols in file and update protocol status in Base if found"""
+    filename, ext = file.split(".")
+    protocol_list = []
+    # print(users_db.fetch())
+    for user in users_db.fetch():
+        print(user)
+    #     protocols = user.get("protocols", [])
+    #     protocol_list.append(protocol["id"] for protocol in protocols)
+    # with zipfile.ZipFile(Path(DOWNLOAD_DIR).joinpath(file), "r") as zip:
+    #     with zip.open(f"{filename}.xml") as file:
+    #         for line in file:
+    #             for protocol in protocol_list:
+    #                 if protocol in line:
+    #                     users_db.
+    return
 
 
 def main():
     try:
-        open_website("http://revistas.inpi.gov.br/rpi/")
-        download_file()
-        search_protocols
+        # open_website("http://revistas.inpi.gov.br/rpi/")
+        # file = download_file()
+        search_protocols("file.t")
     finally:
         browser_lib.close_all_browsers()
     print(f"--- {time.time() - start_time} seconds ---")
